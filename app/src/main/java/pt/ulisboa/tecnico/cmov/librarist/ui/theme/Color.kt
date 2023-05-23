9eeb92a3235c41f007da94f9649957808628dffc
David Bilnica <58438021+dbilnica@users.noreply.github.com>
2023-05-23 02:00:11 +0100

    UI Adaptability: Light/Dark Theme

--- app/src/main/java/pt/ulisboa/tecnico/cmov/librarist/ui/theme/Color.kt
+++ app/src/main/java/pt/ulisboa/tecnico/cmov/librarist/ui/theme/Color.kt
@@ -2,10 +2,66 @@ package pt.ulisboa.tecnico.cmov.librarist.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
+// Light Theme
+val md_theme_light_primary = Color(0xFF1565C0) // Rich Blue
+val md_theme_light_onPrimary = Color(0xFFFFFFFF) // White
+val md_theme_light_primaryContainer = Color(0xFF42A5F5) // Lighter Blue
+val md_theme_light_onPrimaryContainer = Color(0xFF000000) // Black
+val md_theme_light_secondary = Color(0xFF03A9F4) // Light Blue
+val md_theme_light_onSecondary = Color(0xFF000000) // Black
+val md_theme_light_secondaryContainer = Color(0xFF80D8FF) // Lightest Blue
+val md_theme_light_onSecondaryContainer = Color(0xFF000000) // Black
+val md_theme_light_tertiary = Color(0xFF26A69A) // Teal
+val md_theme_light_onTertiary = Color(0xFFFFFFFF) // White
+val md_theme_light_tertiaryContainer = Color(0xFFB2DFDB) // Light Teal
+val md_theme_light_onTertiaryContainer = Color(0xFF000000) // Black
+val md_theme_light_error = Color(0xFFB00020) // Red
+val md_theme_light_onError = Color(0xFFFFFFFF) // White
+val md_theme_light_errorContainer = Color(0xFFFFCDD2) // Light Red
+val md_theme_light_onErrorContainer = Color(0xFF000000) // Black
+val md_theme_light_background = Color(0xFFFFFFFF) // White
+val md_theme_light_onBackground = Color(0xFF000000) // Black
+val md_theme_light_surface = Color(0xFFFFFFFF) // White
+val md_theme_light_onSurface = Color(0xFF000000) // Black
+val md_theme_light_surfaceVariant = Color(0xFFE0E0E0) // Light Grey
+val md_theme_light_onSurfaceVariant = Color(0xFF000000) // Black
+val md_theme_light_outline = Color(0xFF9E9E9E) // Grey
+val md_theme_light_inverseOnSurface = Color(0xFFFFFFFF) // White
+val md_theme_light_inverseSurface = Color(0xFF000000) // Black
+val md_theme_light_inversePrimary = Color(0xFF1565C0) // Rich Blue
+val md_theme_light_shadow = Color(0xFF000000) // Black
+val md_theme_light_surfaceTint = Color(0xFFBDBDBD) // Dark Grey
+val md_theme_light_outlineVariant = Color(0xFF757575) // Darker Grey
+val md_theme_light_scrim = Color(0xFF000000) // Black
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+// Dark Theme
+val md_theme_dark_primary = Color(0xFF0D47A1) // Dark Blue
+val md_theme_dark_onPrimary = Color(0xFFFFFFFF) // White
+val md_theme_dark_primaryContainer = Color(0xFF1E88E5) // Darker Blue
+val md_theme_dark_onPrimaryContainer = Color(0xFFFFFFFF) // White
+val md_theme_dark_secondary = Color(0xFF0277BD) // Dark Light Blue
+val md_theme_dark_onSecondary = Color(0xFFFFFFFF) // White
+val md_theme_dark_secondaryContainer = Color(0xFF40C4FF) // Bright Blue
+val md_theme_dark_onSecondaryContainer = Color(0xFFFFFFFF) // White
+val md_theme_dark_tertiary = Color(0xFF00897B) // Dark Teal
+val md_theme_dark_onTertiary = Color(0xFFFFFFFF) // White
+val md_theme_dark_tertiaryContainer = Color(0xFF20B2AA) // Light Sea Green
+val md_theme_dark_onTertiaryContainer = Color(0xFFFFFFFF) // White
+val md_theme_dark_error = Color(0xFFD32F2F) // Dark Red
+val md_theme_dark_onError = Color(0xFFFFFFFF) // White
+val md_theme_dark_errorContainer = Color(0xFFEF9A9A) // Light Red
+val md_theme_dark_onErrorContainer = Color(0xFF000000) // Black
+val md_theme_dark_background = Color(0xFF121212) // Almost Black
+val md_theme_dark_onBackground = Color(0xFFFFFFFF) // White
+val md_theme_dark_surface = Color(0xFF2A2A2A) // Lighter Grey
+val md_theme_dark_onSurface = Color(0xFFFFFFFF) // White
+val md_theme_dark_surfaceVariant = Color(0xFF333333) // Darker Grey
+val md_theme_dark_onSurfaceVariant = Color(0xFFFFFFFF) // White
+val md_theme_dark_outline = Color(0xFF494949) // Grey
+val md_theme_dark_inverseOnSurface = Color(0xFF000000) // Black
+val md_theme_dark_inverseSurface = Color(0xFFFFFFFF) // White
+val md_theme_dark_inversePrimary = Color(0xFF0D47A1) // Dark Blue
+val md_theme_dark_shadow = Color(0xFFFFFFFF) // White
+val md_theme_dark_surfaceTint = Color(0xFF424242) // Grey
+val md_theme_dark_outlineVariant = Color(0xFF616161) // Light Grey
+val md_theme_dark_scrim = Color(0xFFFFFFFF) // White
