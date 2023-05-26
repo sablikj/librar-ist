2afc9425425be6e68ee366a2297ba023efa0f35a
Tina Petkova <tina5kova@gmail.com>
2023-05-26 16:51:58 +0200

    update

--- app/src/main/java/pt/ulisboa/tecnico/cmov/librarist/notifications/NotificationService.kt
+++ app/src/main/java/pt/ulisboa/tecnico/cmov/librarist/notifications/NotificationService.kt
@@ -4,6 +4,7 @@ import android.util.Log
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.async
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 import pt.ulisboa.tecnico.cmov.librarist.data.remote.LibraryApi
 import pt.ulisboa.tecnico.cmov.librarist.model.Book
@@ -13,28 +14,34 @@ class NotificationService(
     private val libraryApi: LibraryApi,
     private val notificationsController: NotificationsController
 ) {
+    private val pollingIntervalMillis = 5000L // 5 seconds
 
-    suspend fun checkBookAvailability(bookTitle: String) {
+    private fun startApiPolling(notification: Boolean, bookTitle: String) {
         try {
             CoroutineScope(Dispatchers.IO).launch {
-                val response = async {
-                    libraryApi.getBookByTitle(bookTitle)
-                }.await()
+                while (notification) {
+                    val response = async {
+                        libraryApi.getBookByTitle(bookTitle)
+                    }.await()
+                    //handle response
+                    if (response.isSuccessful && response.body() != null) {
+                        val book = response.body() as Book
+                        book.available?.let {
+                            showNotification(
+                                "Book available",
+                                book.name + "is available now",
+                            )
+                        }
 
-                if (response.isSuccessful && response.body() != null) {
-                    val book = response.body() as Book
-                    book.available?.let {
-                        showNotification(
-                            "Book available",
-                            book.name + "is available now",
-                        )
                     }
-
+                    delay(pollingIntervalMillis)
                 }
             }
 
-        } catch (e: Exception) {
-            Log.d("error", "Failed Notification Service call")
+        } catch (e: Throwable) {
+            // Handle network or request failure
+            e.printStackTrace()
+            Log.d("error", "")
         }
     }
 
